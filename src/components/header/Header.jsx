fd27162b6fcf88729d97edf1fce300b071369393
TichaTin <ticha.tin@gmail.com>
2023-01-18 00:14:33 +1100

    setup + header section

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,89 @@
+import React,{useState} from 'react';
+import { Link } from 'react-router-dom';
+import { useMediaQuery } from 'react-responsive';
+
+import './header.css'
+import logo from '../../assets/jl-logo.png';
+
+const BOOKNOW_BTN = "";
+
+const Menu = () =>{
+  return (
+    <>
+    <ul>
+      <li>
+        <a href="#jl-banner">
+          Home
+        </a>
+      </li>
+      <li>
+        <a href="#jl-about">
+          About Us
+        </a>
+      </li>
+      <li>
+        <a href="#jl-conditions">
+          Conditions
+        </a>
+      </li>
+      <li>
+        <a href="#jl-contact">
+          Contact
+        </a>
+      </li>
+    </ul>
+    </>
+  )
+}
+
+const Header = () => {
+  const isNotMobile = useMediaQuery({ minWidth: 767 })
+  const isDeskTop = useMediaQuery({ minWidth: 1024 })
+  const isNotDeskTop = useMediaQuery({ maxWidth: 1023 })
+  const [navToggle, setnavToggle] = useState(false);
+
+  console.log(navToggle)
+
+  return (
+    <header className='header section__padding'>
+      <div className='mx-auto'>
+        <div className='header__wrapper w-full'>
+
+          <Link
+            to='/'
+            className='block m-2 header-logo'
+          >
+            <img src={logo} alt="logo" 
+            />
+          </Link>
+
+          {isNotMobile && (
+            <nav className='header__nav'>
+              {(navToggle & isNotDeskTop) 
+                ? <button className='header__nav--icon toggled' onClick={() => {setnavToggle(false)}}></button>
+                : <button className='header__nav--icon' onClick={() => {setnavToggle(true)}}></button>
+              }
+              {(navToggle & isNotDeskTop)
+                ? Menu()
+                : ''
+              }
+              {isDeskTop && Menu()}
+              
+            </nav>
+          )}
+          
+          <div className='header__contact'>
+            <a href={BOOKNOW_BTN}
+              className='m-1 mx-2 mt-4 btn btn-trans lg:ml-4'
+            >
+              BOOK ONLINE
+            </a>
+          </div>
+        </div>
+        
+      </div>
+    </header>
+  )
+}
+
+export default Header
---
+++ src/containers/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Header} from '../components/index';
+
+const PAGE_NAME = `page-home`;
+
+
+const Home = () => {
+  return (
+    <div className={PAGE_NAME}>
+      <Header/>
+    </div>
+  )
+}
+
+export default Home
