b2b8ce6a280340741b6e74bbd774c962a90539e8
TichaTin <ticha.tin@gmail.com>
2023-01-18 00:47:35 +1100

    adding section id

--- src/components/homeBanner/HomeBanner.jsx
+++ src/components/homeBanner/HomeBanner.jsx
@@ -8,7 +8,7 @@ const HomeBanner = () => {
   const isMobile = useMediaQuery({ maxWidth: 550 })
 
   return (
-    <section className='home-banner section__padding flex items-center align-center text-center' style={{ backgroundImage: `url(${BANNER_IMG})` }}>
+    <section id='#jl-banner' className='home-banner section__padding flex items-center align-center text-center' style={{ backgroundImage: `url(${BANNER_IMG})` }}>
       <div className='container mx-auto md:text-right'>
         <h1 dangerouslySetInnerHTML={{__html: content.banner.h1}}></h1>
       </div>
